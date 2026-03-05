0a3d648c1678deb4b49e02b4e43d86190e3b7e59
shubham-codeass <shubham.julyventures@gmail.com>
2026-03-05 14:17:53 +0530

    Create Next.js Document setup in pages/_document.tsx

---
+++ pages/_document.tsx
@@ -0,0 +1,23 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta charSet="UTF-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <meta name="description" content="VC Portfolio Tracker - Track your venture capital investments" />
+          <link rel="icon" href="/favicon.ico" />
+          <title>VC Portfolio Tracker</title>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
